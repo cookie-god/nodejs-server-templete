d4369925ae6861e217eb4e80d11f81a71cf7e891
cookie-god <lion0193@gmail.com>
2020-12-29 19:10:34 +0900

    init node server templete

--- database.js
+++ database.js
@@ -4,8 +4,8 @@ const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     port: 3306,
-    password: 'kooki7869^^',
-    database: 'testdb'
+    password: '',
+    database: ''
 });
 
 module.exports = {
